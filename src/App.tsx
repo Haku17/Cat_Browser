c87688f7cd3131d93505230db23d8732b266cade
jordan <jordan@hakustudios.com>
2023-09-18 15:09:04 +0300

    Installed react-router-dom and setup the two initial routes: HomePage and SingleCatPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./routes/HomePage";
+import SingleCatPage from "./routes/SingleCatPage";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Cat Browser</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/breed/:breedId" element={<SingleCatPage />} />
+    </Routes>
   );
 }
 
---
+++ src/routes/SingleCatPage.tsx
@@ -0,0 +1,9 @@
+const SingleCatPage = () => {
+  return (
+    <>
+      <h1>Single Cat Page!</h1>
+    </>
+  );
+};
+
+export default SingleCatPage;
