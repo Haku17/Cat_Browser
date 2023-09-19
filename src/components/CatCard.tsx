4cce9eb32b2f832dd30f79dc68148404615063bd
jordan <jordan@hakustudios.com>
2023-09-19 14:09:05 +0300

    added Load More button to CatCardList. Loads more cat photos from the array until it runs out, then removes the button.

--- src/components/CatCard.tsx
+++ src/components/CatCard.tsx
@@ -1,9 +1,22 @@
+import { CSSProperties } from "react";
 import { Link } from "react-router-dom";
+
+const ImgStyles: CSSProperties = {
+  width: "200px",
+  height: "200px",
+  objectFit: "cover",
+  objectPosition: "center",
+};
+
+const DivStyles: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+};
 
 const CatCard = ({ id, url }: { id: string; url: string }) => {
   return (
-    <div>
-      <img src={url} />
+    <div style={DivStyles}>
+      <img src={url} style={ImgStyles} />
       <Link to={`breed/${id}`}>Learn More</Link>
     </div>
   );
--- src/components/CatCardList.tsx
+++ src/components/CatCardList.tsx
@@ -1,5 +1,14 @@
-import { useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 import CatCard from "./CatCard";
+
+const ContainerStyles: CSSProperties = {
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "center",
+  alignItems: "start",
+  width: "100%",
+  gap: "10px",
+};
 
 type CatPhotoProps = {
   id: string;
@@ -10,6 +19,17 @@ type CatPhotoProps = {
 
 const CatCardList = ({ catId }: { catId: string }) => {
   const [catPhotos, setCatPhotos] = useState<CatPhotoProps[]>([]);
+  const [displayedPhotos, setDisplayedPhotos] = useState<CatPhotoProps[]>([]);
+  const [startIndex, setStartIndex] = useState(0);
+
+  // Function to load the next set of photos
+  const loadNextPhotos = () => {
+    const endIndex = startIndex + 3;
+    setDisplayedPhotos(catPhotos.slice(0, endIndex));
+    setStartIndex(endIndex);
+    console.log("catPhotos: ", catPhotos.length);
+    console.log("startIndex ", startIndex);
+  };
 
   // Fetches images of the selected cat breed by catId, then sets them to state variable (catPhoto) to be rendered below by CatCard component
   useEffect(() => {
@@ -20,18 +40,31 @@ const CatCardList = ({ catId }: { catId: string }) => {
       `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${catId}`
     )
       .then((response) => response.json())
-      .then((data) => setCatPhotos(data))
+      .then((data) => {
+        setCatPhotos(data);
+        // reset start index if new catPhotos
+        setStartIndex(0);
+      })
       .catch((e) => console.log(e));
   }, [catId]);
+
+  // Inital call of loadNextPhots(), resets if catPhotos array changes
+  useEffect(() => {
+    loadNextPhotos();
+  }, [catPhotos]);
 
   return (
     <>
       {!catId && <p>Select a breed to see more cats!</p>}
-      <div>
-        {catPhotos.map((photo) => (
+      <div style={ContainerStyles}>
+        {displayedPhotos.map((photo) => (
           <CatCard key={photo.id} id={photo.id} url={photo.url} />
         ))}
       </div>
+      {/* show button as long as there are more photos in the array */}
+      {startIndex <= catPhotos.length && (
+        <button onClick={loadNextPhotos}>Load More 😼</button>
+      )}
     </>
   );
 };
